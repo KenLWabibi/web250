407e77d2715cd18f866b35822b32bf6bae74e465
KenLWabibi <157647708+KenLWabibi@users.noreply.github.com>
2025-04-17 00:17:33 -0400

    Add files via upload

---
+++ php/components/footer.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<footer>
+    <nav>
+        <ul>
+            <li><a href="https://github.com/KenLWabibi">GitHub</a></li> 💧
+            <li><a href="https://KenLWabibi.github.io/">GitHub.io</a></li> 💧
+            <li><a href="https://KenLWabibi.github.io/web115/">WEB115</a></li> 💧
+            <li><a href="https://KenLWabibi.github.io/web250/">WEB250</a></li> 💧
+            <li><a href="https://www.freecodecamp.org/poacode">freeCodeCamp</a></li> 💧
+            <li><a href="https://www.codecademy.com/profiles/Poacode">Codecademy</a></li> 💧
+            <li><a href="https://jsfiddle.net/u/Poacode">JSFiddle</a></li> 💧
+            <li><a href="https://www.linkedin.com/in/ken-wabibi-715158a2/">Linkedin</a></li>
+        </ul>
+    </nav>
+    <p>Website designed by Wabibi Industries | Certified in <a href="https://freecodecamp.org/certification/poacode/responsive-web-design">RWD</a></p>
+</footer>
---
+++ php/content/introduction.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>Ken Wabibi's Keen Whale 🐋 Introduction</title>
+    <link href="styles/introduction.css" rel="stylesheet">
+    <script src="https://lint.page/kit/67ff88.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <?php include 'components/header.php'; ?>
+    <main>
+        <h2>Introduction</h2>
+        <figure>
+            <img src="ck_resized3.png"  alt="Photo of Ken hugging which his black lab, Chance" class="center"> 
+            <figcaption><em>With my dog Chance</em></figcaption>
+        </figure> 
+        <ul>
+            <li><strong>Personal background:</strong> Born and raised in Charlotte, NC. I’m a unicorn.</li>
+            <li><strong>Academic background:</strong> I graduated from Olympic High School then attended Livingstone College for a year then transferred to UNCG but that didn’t work out. Now I’m CPCC and my major is Full Stack Programming.</li>
+            <li><strong>Background in this subject:</strong> I’m new to programming. I took my first programming class which was C++ Fall 2023 and fell in love with it.</li>
+            <li><strong>Primary Computer Platform:</strong> macOS Monterey laptop</li>
+            <li><strong>Courses I'm Taking &amp; Why:</strong>
+                 <ul>
+                        <li><strong>CSC221 Advanced Python</strong> - It’s required for my major, but I also want to improve my python programming skills.</li>
+                        <li><strong>CTS240 Project Management</strong> - This class will help me get an inside look on how to work with a team to get a project done using project management software.</li>
+                        <li><strong>WEB115 Web Markup and Scripting</strong> - I want to better my understanding of  front-end programming.</li>
+                        <li><strong>WEB140 Web Development Tools</strong> - It’s required for my major, but I'm interested about the new knowledge I’m going to gain.</li>
+                        <li><strong><a href="contract.html">WEB250 Database Driven Websites</a></strong> - I want to learn about CRUD. I see CRUD being mentioned on job descriptions and curious about what it is.</li>
+                 </ul>   
+            </li>
+            <li><strong>Funny/Interesting item about yourself:</strong> I’m a night owl. I’m most creative at night. I can go to bed at 3am and be up by 5am with no problems. I try not to do it often, but I have the ability to do so….</li>
+            <li><strong>I'd also like to share:</strong> I’m a foodie.</li>
+        </ul>  
+    </main>
+    <?php include 'components/footer.php'; ?>
+</body>
+</html>
