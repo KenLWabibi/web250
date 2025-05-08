eda25013e750e8b05bb234f9cf0fb01ed050a673
KenLWabibi <157647708+KenLWabibi@users.noreply.github.com>
2025-05-08 07:46:50 -0400

    Update header.php

--- php/components/header.php
+++ php/components/header.php
@@ -1,14 +1,18 @@
-<!DOCTYPE html>
-
 <header>
-        <h1>Ken Wabibi's Keen Whale 🐋 WEB250</h1>
-        <nav>
-            <ul>
-                <li><a href="index.php">Home</a></li> 💧
-                <li><a href="introduction.php">Introduction</a></li> 💧
-                <li><a href="contract.php">Course Contract</a></li> 💧
-                <li><a href="multipage_sites/">Multipage Sites</a></li> 💧
-                <li><a href="joy/">The Joy of PHP</a></li>
-            </ul>
-        </nav>
+    <h1>Ken Wabibi's Keen Whale 🐋 WEB250</h1>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li> 
+            <li><a href="introduction.php">Introduction</a></li> 
+            <li><a href="contract.php">Course Contract</a></li> 
+            <li class="dropdown">
+                <a href="#" class="dropbtn">Multipage Sites ▾</a>
+                <div class="dropdown-content">
+                    <a href="multipage_sites/superduper_php">Superduper PHP</a>
+                    <a href="multipage_sites/superduper_static">Superduper Static</a>
+                </div>
+            </li> 
+            <li><a href="joy/">The Joy of PHP</a></li>
+        </ul>
+    </nav>
 </header>
